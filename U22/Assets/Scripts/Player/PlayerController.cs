db16554593493156d903905086cd7271e3ce67b6
MYCOMPUTER\tokir <toki.ryuka.senriyama@gmail.com>
2019-06-04 15:58:51 +0900

    GameSceneのプレイヤーを管理するクラスを作成

--- U22/Assets/Scripts/Player/PlayerController.cs
+++ U22/Assets/Scripts/Player/PlayerController.cs
@@ -1,7 +1,15 @@
 ﻿using UnityEngine;
 
+/// <summary>
+/// プレイヤーを制御するクラス
+/// </summary>
 public class PlayerController : MonoBehaviour
 {
+    //プレイヤーの番号
+    int m_ThisPlayerNumber = 0;
+    //プレイヤーの番号のプロパティ
+    public int ThisPlayerNumber { set { this.m_ThisPlayerNumber = value; } }
+
     void Start()
     {
 
--- U22/Assets/Scripts/Player/PlayerManager.cs
+++ U22/Assets/Scripts/Player/PlayerManager.cs
@@ -2,21 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// プレイヤーを管理するクラス
+/// </summary>
 public class PlayerManager : MonoBehaviour
 {
     [SerializeField, Header("プレイヤーオブジェクト")]
     GameObject m_PlayerObject = null;
-
+    [SerializeField, Header("プレイヤーの生成位置")]
+    Vector3[] m_PlayerGeneratePosition = null;
+    //プレイヤーリスト
     PlayerController[] m_PlayerControllers = null;
 
 
     void Start()
     {
         m_PlayerControllers = new PlayerController[PlayerNumberCheckManager.m_PlayerNumber];
+        GeneratePlayers(PlayerNumberCheckManager.m_PlayerNumber);
     }
 
     void Update()
     {
 
+    }
+
+    /// <summary>
+    /// プレイヤーを生成する
+    /// </summary>
+    /// <param name="_PlayerNum">プレイヤーの数</param>
+    void GeneratePlayers(int _PlayerNum)
+    {
+        if (!m_PlayerObject.GetComponent<PlayerController>())
+        {
+            Debug.LogError("PlayerControllerがついてません");
+            return;
+        }
+
+        GameObject playerParentObject = new GameObject("playerList");
+
+        //プレイヤーを生成
+        for (int i = 0; i < _PlayerNum; ++i)
+        {
+            m_PlayerControllers[i] =
+                Instantiate(m_PlayerObject, m_PlayerGeneratePosition[i], Quaternion.identity).
+                GetComponent<PlayerController>();
+            //番号をセット
+            m_PlayerControllers[i].ThisPlayerNumber = i;
+            //親オブジェクトにまとめる
+            m_PlayerControllers[i].transform.parent = playerParentObject.transform;
+        }
     }
 }
