d7b03e9463b6848e5f0708a60d6c9f7fb01d2eeb
MYCOMPUTER\tokir <toki.ryuka.senriyama@gmail.com>
2019-06-05 10:56:42 +0900

    仮のプレイヤーを作成

--- U22/Assets/Scripts/Config/Config.cs
+++ U22/Assets/Scripts/Config/Config.cs
@@ -14,25 +14,11 @@ public class Config : Singleton<Config>
     /// <value>プレイヤーオブジェクト</value>
     public GameObject PlayerObject { get { return this.m_PlayerObject; } }
 
-
-    [SerializeField, Header("プレイヤーの移動速度")]
-    float m_PlayerMoveSpeed = 0.0f;
-
-    /// <summary>
-    /// プレイヤーの移動速度のプロパティ
-    /// </summary>
-    /// <value>プレイヤーの移動速度</value>
-    public float PlayerMoveSpeed { get { return this.m_PlayerMoveSpeed; } }
-
     public override void MyStart()
     {
         if (!m_PlayerObject.GetComponent<PlayerController>())
         {
             Debug.LogError("プレイヤーオブジェクトにPlayerControllerがComponentされていません");
-        }
-        if (m_PlayerMoveSpeed <= 0.0f)
-        {
-            Debug.LogError("プレイヤーの移動速度が0以下です");
         }
     }
 }
--- U22/Assets/Scripts/Player/PlayerController.cs
+++ U22/Assets/Scripts/Player/PlayerController.cs
@@ -10,14 +10,114 @@ public class PlayerController : MonoBehaviour
     //プレイヤーの番号のプロパティ
     public int ThisPlayerNumber { set { this.m_ThisPlayerNumber = value; } }
 
+    [SerializeField, Header("プレイヤーの移動速度")]
+    float m_PlayerMoveSpeed = 0.0f;
+    [SerializeField, Header("腕の制御")]
+    ArmController m_ArmController = new ArmController();
+
     void Start()
     {
-
+        if (m_PlayerMoveSpeed <= 0.0f)
+        {
+            Debug.LogError("プレイヤーの移動速度が0以下です");
+        }
+        m_ArmController.ArmInitialize(m_ThisPlayerNumber);
     }
 
     void Update()
     {
+        Movement();
+        m_ArmController.ArmUpdate();
+    }
+
+    /// <summary>
+    /// 移動処理
+    /// </summary>
+    void Movement()
+    {
+        //移動量
         var moveValue = new Vector3(SwitchInput.GetHorizontal(m_ThisPlayerNumber), SwitchInput.GetVertical(m_ThisPlayerNumber), 0.0f);
-        transform.Translate(moveValue * Time.deltaTime * Config.GetInstance().PlayerMoveSpeed);
+        //移動
+        transform.Translate(moveValue * Time.deltaTime * m_PlayerMoveSpeed);
+    }
+}
+
+/// <summary>
+/// 腕の制御
+/// </summary>
+[System.Serializable]
+public class ArmController
+{
+    //現在の回転(z軸)
+    float m_RotationValue = 0.0f;
+    [SerializeField, Header("回転速度")]
+    float m_RotationSpeed = 0.0f;
+    [SerializeField, Header("最小角度")]
+    float m_MinAngle = 0.0f;
+    [SerializeField, Header("最大角度")]
+    float m_MaxAngle = 0.0f;
+    [SerializeField, Header("右腕")]
+    GameObject m_RightArmObject = null;
+    [SerializeField, Header("左腕")]
+    GameObject m_LeftArmObject = null;
+
+    int m_PlayerNumber = 0;
+
+    /// <summary>
+    /// 腕の初期化
+    /// </summary>
+    /// <param name="_PlayerNumber">プレイヤーの番号</param>
+    public void ArmInitialize(int _PlayerNumber)
+    {
+        m_PlayerNumber = _PlayerNumber;
+        //エラーチェックやNullチェック
+        if (m_RotationSpeed <= 0.0f)
+        {
+            Debug.LogError("腕の回転速度が0以下です");
+        }
+        if (m_MinAngle > m_MaxAngle)
+        {
+            Debug.LogError("最小角度が最大角度より大きいです");
+        }
+        if (!m_RightArmObject)
+        {
+            Debug.LogError("右腕のオブジェクトがありません");
+        }
+        if (!m_LeftArmObject)
+        {
+            Debug.LogError("左腕のオブジェクトがありません");
+        }
+    }
+
+    /// <summary>
+    /// 腕の更新
+    /// </summary>
+    public void ArmUpdate()
+    {
+        ArmRotation();
+    }
+
+    /// <summary>
+    /// 回転処理
+    /// </summary>
+    void ArmRotation()
+    {
+        //Rなら開く
+        if (SwitchInput.GetButton(m_PlayerNumber, SwitchButton.SR))
+        {
+            m_RotationValue += m_RotationSpeed * Time.deltaTime;
+            Debug.Log("sr");
+        }
+        //Lなら閉じる
+        if (SwitchInput.GetButton(m_PlayerNumber, SwitchButton.SL))
+        {
+            m_RotationValue -= m_RotationSpeed * Time.deltaTime;
+            Debug.Log("sl");
+        }
+        //最小、最大角度にClamp
+        m_RotationValue = Mathf.Clamp(m_RotationValue, m_MinAngle, m_MaxAngle);
+        //角度を設定
+        m_RightArmObject.transform.localRotation = Quaternion.Euler(0.0f, 0.0f, -m_RotationValue);
+        m_LeftArmObject.transform.localRotation = Quaternion.Euler(0.0f, 0.0f, m_RotationValue);
     }
 }
