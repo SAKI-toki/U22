1734288b0f40bf7f4e5425625a41afbeb8d9b236
RyogaOkuchi <1121ryoga@gmail.com>
2019-06-06 12:30:38 +0900

    追跡する敵完成

--- U22/Assets/Scripts/Enemy/TrackingEnemy.cs
+++ U22/Assets/Scripts/Enemy/TrackingEnemy.cs
@@ -10,45 +10,72 @@ public class TrackingEnemy : MonoBehaviour
     //画面範囲の上限と下限
     Vector2 m_DisplayMin;
     Vector2 m_DisplayMax;
-    Vector2 vecDifference;
+    Vector2 m_VecDifference;
+    Vector2 m_RandomMoveVec;
 
     [SerializeField, Range(0, 360), Header("最大視野角")]
     int m_MaxSight;
 
+    float m_RandomMoveInterval = 0;
     float m_Angle;
-    const float m_Speed = 2;
+    const float m_Speed = 1;
+
+    bool isMoving;
+
     // Start is called before the first frame update
     void Start()
     {
         m_DisplayMax = Camera.main.ViewportToWorldPoint(Vector2.one);
         m_DisplayMin = Camera.main.ViewportToWorldPoint(Vector2.zero);
 
-        InvokeRepeating("NearPlayerSearch", 0f, 5f);
+        RandomMove();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        NearPlayerSearch();
+        //追跡するターゲットがNull以外だったら
         if (m_Target != null)
         {
             //ターゲットへの方向ベクトルを求める
-            vecDifference = (m_Target.transform.position - transform.position).normalized;
+            m_VecDifference = (m_Target.transform.position - transform.position).normalized;
 
             //敵の向いている方向からターゲットへの角度を求める
-            m_Angle = Vector2.Angle(transform.up, vecDifference);
+            m_Angle = Vector2.Angle(transform.up, m_VecDifference);
 
-            //ターゲットへの角度が視野角以内,だったら追尾を開始する
-            if (m_Angle < m_MaxSight / 2 && Vector2.Distance(transform.position,m_Target.transform.position) < 3)
+            //ターゲットへの角度が視野角以内だったら追尾を開始する
+            if (m_Angle < m_MaxSight / 2)
             {
-                transform.up = Vector2.Lerp(transform.up, vecDifference, Time.deltaTime * 10);
+                transform.up = Vector2.Lerp(transform.up, m_VecDifference, Time.deltaTime * 10);
             }
         }
+        else
+        {
+            m_RandomMoveInterval += Time.deltaTime;
+            if (m_RandomMoveInterval > 2f)
+            {
+                if (!isMoving)
+                {
+                    RandomMove();
+                    isMoving = !isMoving;
+                }
+                else
+                {
+                    isMoving = !isMoving;
+                }
+                m_RandomMoveInterval = 0;
+            }
+            transform.up = Vector2.Lerp(transform.up, m_RandomMoveVec, Time.deltaTime * 5);
+        }
+        if (!isMoving)
+        {
+            transform.position += transform.up.normalized * Time.deltaTime * m_Speed;
+        }
 
-        transform.position += transform.up * Time.deltaTime * m_Speed;
     }
 
-    //このオブジェクトと一番距離が近いPlayerを探し、返す。
+    //敵と一番距離が近いPlayerを探す
     void NearPlayerSearch()
     {
         float nearDistance = 0;
@@ -59,12 +86,25 @@ public class TrackingEnemy : MonoBehaviour
         {
             float distance = Vector2.Distance(this.gameObject.transform.position, player.transform.position);
 
-            if (nearDistance == 0 || nearDistance > distance)
+            if ((nearDistance == 0 || nearDistance > distance) && distance < 3)
             {
                 nearDistance = distance;
                 targetPlayer = player;
+                isMoving = false;
             }
         }
         m_Target = targetPlayer;
+    }
+
+    //敵のランダム移動
+    void RandomMove()
+    {
+        //ランダム方向のベクトルを格納
+        Vector2 randomMoveVec = Vector2.zero;
+        //画面内のランダムな位置を所得
+        randomMoveVec = new Vector2(Random.Range(m_DisplayMax.x, m_DisplayMin.x), Random.Range(m_DisplayMax.y, m_DisplayMin.y));
+
+        m_RandomMoveVec = randomMoveVec;
+
     }
 }
